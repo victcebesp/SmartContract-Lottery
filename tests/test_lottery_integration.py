e04ae60455dc9c6d2b7e23b8e460f348a7abee48
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2021-12-28 13:46:28 +0000

    Initial commit

---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,22 @@
+from decimal import localcontext
+from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, fund_with_link, get_account
+from brownie import network
+import pytest
+import time 
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee() + 1000})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee() + 1000})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(60)
+
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
